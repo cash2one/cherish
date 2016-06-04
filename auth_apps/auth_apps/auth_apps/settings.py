1b25c71cdf793302e06aad2a6fc2354000ed489e
xuchongbo <xcbfreedom@gmail.com>
2016-06-04 22:23:15 +0800

    fix typo

--- auth_apps/auth_apps/auth_apps/settings.py
+++ auth_apps/auth_apps/auth_apps/settings.py
@@ -142,12 +142,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 DEFAULT_FILE_STORAGE = 'db_file_storage.storage.DatabaseFileStorage'
 
 # email settings
-EMAIL_USE_SSL = int(os.getenv('EMAIL_USE_SSL'))
-EMAIL_USE_TLS = int(os.getenv('EMAIL_USE_TLS'))
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_USE_SSL = int(os.getenv('EMAIL_USE_SSL', 0))
+EMAIL_USE_TLS = int(os.getenv('EMAIL_USE_TLS', 0))
+EMAIL_HOST = os.getenv('EMAIL_HOST', None)
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', None)
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', None)
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 0))
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # auth setting
