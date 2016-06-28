186c74032ed7d5eeb4ebaeb547b4e714892c1f39
lanbijia <lbj.world@gmail.com>
2016-06-28 23:24:41 +0800

    FIX : upload avatar

--- auth_apps/auth_apps/auth_user/widgets.py
+++ auth_apps/auth_apps/auth_user/widgets.py
@@ -1,26 +1,14 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
 from django import forms
-from django.utils.safestring import mark_safe
 from db_file_storage.form_widgets import db_file_widget
 
 
-class AdminImageWidget(forms.FileInput):
-    """
-    A ImageField Widget for admin that shows a thumbnail.
-    """
-
-    def __init__(self, attrs={}):
-        super(AdminImageWidget, self).__init__(attrs)
-
-    def render(self, name, value, attrs=None):
-        output = []
-        if value and hasattr(value, "url"):
-            output.append(('<a target="_blank" href="%s">'
-                           '<img src="%s" style="height: 28px;" /></a> '
-                           % (value.url, value.url)))
-        output.append(super(AdminImageWidget, self).render(name, value, attrs))
-        return mark_safe(u''.join(output))
-
-
 @db_file_widget
-class DBAdminImageWidget(AdminImageWidget):
-    pass
+class DBImageInputWidget(forms.ClearableFileInput):
+    template_with_initial = (
+        '%(initial_text)s: <a target="_blank" href="%(initial_url)s">'
+        '<img src="%(initial_url)s" style="height: 28px;" alt="%(initial)s"/></a> '
+        '%(clear_template)s<br />%(input_text)s: %(input)s'
+    )
