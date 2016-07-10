b0fefa7422b7384c5a54574feeee3a29c184b48d
lanbijia <lbj.world@gmail.com>
2016-07-10 23:18:14 +0800

    OPTIMIZE : check key before set remote addr

--- auth_apps/auth_apps/common/middleware.py
+++ auth_apps/auth_apps/common/middleware.py
@@ -8,9 +8,10 @@ logger = logging.getLogger(__name__)
 
 class SetRemoteAddrMiddleware(object):
     def process_request(self, request):
-        try:
-            request.META['REMOTE_ADDR'] = request.META['HTTP_X_REAL_IP']
-        except:
-            request.META['REMOTE_ADDR'] = '1.1.1.1'
+        if 'HTTP_X_REAL_IP' in request.META:
+            try:
+                request.META['REMOTE_ADDR'] = request.META['HTTP_X_REAL_IP']
+            except:
+                request.META['REMOTE_ADDR'] = '1.1.1.1'
         logger.debug('[SetRemoteAddrMiddleware] remote addr ({ip})'.format(
             ip=request.META['REMOTE_ADDR']))
