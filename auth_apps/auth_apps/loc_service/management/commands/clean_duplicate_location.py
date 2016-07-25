1aab137d4c0717208d9a0de6b71fd3b37e875c9e
Haeryong Ding <mongoo@sina.cn>
2016-07-25 20:24:30 +0800

    fix: use django model

--- auth_apps/auth_apps/loc_service/management/commands/clean_duplicate_location.py
+++ auth_apps/auth_apps/loc_service/management/commands/clean_duplicate_location.py
@@ -2,45 +2,40 @@
 
 from collections import defaultdict
 
-from django.db import connection
 from django.core.management.base import BaseCommand
+from django.db import transaction
+
+from loc_service.models import Location
+from edu_info.models import School
 
 
 class Command(BaseCommand):
 
-    def dictfetchall(self, cursor):
-        columns = [col[0] for col in cursor.description]
-        return [
-            dict(zip(columns, row)) for row in cursor.fetchall()
-        ]
-
     def handle(self, *args, **options):
-        sql = "select code, name, parent_id as p_code from loc_service_location"
+
         name_codes = defaultdict(list)
-        with connection.cursor() as c:
-            c.execute(sql)
 
-            for item in self.dictfetchall(c):
-                code = item.get('code')
-                name_unicode = item.get('name')
-                p_code = item.get('p_code')
-                name_utf8 = name_unicode.encode('utf8')
-                _name = ''.join(name_unicode.split())
+        for location in Location.objects.all():
+            name = location.name
+            _name = ''.join(name.split())
 
-                if len(name_utf8) != len(_name) and len(name_utf8) / len(_name) != 3:
-                    self.stderr.write(u'illegal location: {c} {n}'.format(c=code, n=name_unicode))
-                    sql = 'update loc_service_location set name={n} where code={c}'.format(c=code, n=_name)
-                    c.execute(sql)
+            if name != _name:
+                self.stderr.write(u'illegal location: {c} {n}'.format(
+                    c=location.code, n=location.name))
+                location.name = _name
+                location.save()
 
-                name_pcode = (_name, p_code)
-                name_codes[name_pcode].append(code)
+            name_pcode = (_name, location.parent_id)
+            name_codes[name_pcode].append(location.code)
 
-            duplicate_codes_list = filter(lambda x: len(x) > 1, name_codes.values())
+        duplicate_codes_list = filter(lambda x: len(x) > 1, name_codes.values())
 
-            for codes in duplicate_codes_list:
-                assert len(codes) == 2
-                code, _code = codes
-                sql = 'update edu_info_school set area_code_id={c} where area_code_id={_c}'.format(c=code, _c=_code)
-                c.execute(sql)
-                sql = 'delete from loc_service_location where code={c}'.format(c=_code)
-                c.execute(sql)
+        for codes in duplicate_codes_list:
+            code, codes = codes[0], codes[1:]
+
+            with transaction.atomic():
+                for school in School.objects.filter(area_code_id__in=codes):
+                    school.area_code_id = code
+                    school.save()
+                for location in Location.objects.filter(code__in=codes):
+                    location.delete()
