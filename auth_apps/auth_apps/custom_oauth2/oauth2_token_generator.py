ba08396afd7dfe9c9343015841cb766aa47397d7
lanbijia <lbj.world@gmail.com>
2016-08-18 22:26:41 +0800

    FIX: fix orm query

--- auth_apps/auth_apps/custom_oauth2/oauth2_token_generator.py
+++ auth_apps/auth_apps/custom_oauth2/oauth2_token_generator.py
@@ -7,8 +7,8 @@ from oauth2_provider.models import AccessToken
 
 
 def techu_token_generator(request, refresh_token=False):
-    access_tokens = AccessToken.objects.filter(user=request.user,
-                                               application=request.client).order_by('-id')
-    if access_tokens:
-        return access_tokens[0].token
+    access_token = AccessToken.objects.filter(user=request.user,
+                                              application=request.client).order_by('-id').first()
+    if access_token:
+        return access_token.token
     return common.generate_token()
