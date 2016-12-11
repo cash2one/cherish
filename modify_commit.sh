2697e7fb3e6a22aa189307c247892321e2ec93d4
Haeryong Ding <mongoo@sina.cn>
2016-12-11 18:18:27 +0800

    akjsdlf

--- modify_commit.sh
+++ modify_commit.sh
@@ -5,7 +5,7 @@ date="$2"
 name="$3"
 email="$4"
 
-git filter-branch --env-filter \
+git filter-branch -f --env-filter \
     "if [ \$GIT_COMMIT = '$commit' ]; then
          export GIT_AUTHOR_DATE='$date'
          export GIT_AUTHOR_NAME='$name'
